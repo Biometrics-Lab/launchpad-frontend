b77b30ee43b11e4891e13ab72929af015adf8f91
roman.vakulenko <mail@vacoola.neme>
2026-05-25 02:10:28 -0500

    [BL-55] Add status field to SessionType

--- src/types/app/assessmentTypes.ts
+++ src/types/app/assessmentTypes.ts
@@ -4,7 +4,7 @@ export type MeasurementType = {
 }
 
 export type AssessmentType = { id: number; playerId: number; sport: string; templateId: number; conditionId?: number; allowExternalUrls: boolean }
-export type SessionType = { id: number; assessmentId: number; startTime: string }
+export type SessionType = { id: number; assessmentId: number; startTime: string; status?: 'ACTIVE' | 'COMPLETE' | null }
 export type RepType = { id: number; sessionId: number; startTime: string }
 export type AssessmentMetricType = { id: number; assessmentId: number; conditionalMetricId: number; sourceId: number; description?: string; minValue?: number; maxValue?: number; avgValue?: number; lastValue?: number }
 export type SessionMetricType = { id: number; sessionId: number; conditionalMetricId: number; minValue?: number; maxValue?: number; avgValue?: number }
